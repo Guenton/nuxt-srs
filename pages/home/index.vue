eedcb8cc50c3bdcc7acf2eecf2f27be99f6533df
Guenton Brooks <Guenton@Gmail.com>
2019-09-03 10:25:19 -0400

    first commit

---
+++ pages/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      Under Construction
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        Statistical Report System
+      </h1>
+      <h2 class="subtitle">
+        Redifining Statistical Reports
+      </h2>
+      <div class="links">
+        <b-button to="/home" variant="success" size="lg">
+          Start
+        </b-button>
+        <b-button to="/docs" size="lg">
+          Documentation
+        </b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
