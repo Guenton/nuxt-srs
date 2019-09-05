b3c739eed2fef3d41072dc55eed6ddaa0906949a
Guenton Brooks <Guenton@Gmail.com>
2019-09-05 00:27:04 -0400

    got eslintrc configured

---
+++ components/NavbarHome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand to="/home">S&I SRS</b-navbar-brand>
+      <b-navbar-nav>
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Add" right>
+          <b-dropdown-item to="/subs">Subsidiaries</b-dropdown-item>
+          <b-dropdown-item to="/employees">Employees</b-dropdown-item>
+          <b-dropdown-item to="/services">Services</b-dropdown-item>
+          <b-dropdown-item to="/tasks">Tasks</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown text="Edit" right>
+          <b-dropdown-item to="/subs">Subsidiaries</b-dropdown-item>
+          <b-dropdown-item to="/employees">Employees</b-dropdown-item>
+          <b-dropdown-item to="/services">Services</b-dropdown-item>
+          <b-dropdown-item to="/tasks">Tasks</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown text="Remove" right>
+          <b-dropdown-item to="/subs">Subsidiaries</b-dropdown-item>
+          <b-dropdown-item to="/employees">Employees</b-dropdown-item>
+          <b-dropdown-item to="/services">Services</b-dropdown-item>
+          <b-dropdown-item to="/tasks">Tasks</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <h1>
-      Under Construction
-    </h1>
+    <NavbarHome />
+    <CarouselHome />
   </div>
 </template>
 
 <script>
+import NavbarHome from "~/components/NavbarHome";
+import CarouselHome from "~/components/CarouselHome";
 export default {
-
-}
+  components: {
+    NavbarHome,
+    CarouselHome
+  }
+};
 </script>
-
-<style>
-
-</style>
