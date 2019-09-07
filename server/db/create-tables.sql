85f1f00d8b4275c0dc559f32e1f733dd7a20bdad
Guenton Brooks <Guenton@Gmail.com>
2019-09-07 00:25:54 -0400

    added db table creation file

---
+++ server/db/create-tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS employee (
+  emp_id INT AUTO_INCREMENT PRIMARY KEY,
+  firstname varchar(50) NOT NULL,
+  lastname varchar(50) NOT NULL,
+  pos_id INT DEFAULT NULL,
+  sub_id INT DEFAULT NULL,
+  UNIQUE INDEX unique_fullname (firstname, lastname)
+);
+
+CREATE TABLE IF NOT EXISTS sub (
+  sub_id INT AUTO_INCREMENT PRIMARY KEY,
+  shorthand VARCHAR(25) NOT NULL,
+  location VARCHAR(25) NOT NULL,
+  title VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS position (
+  pos_id INT AUTO_INCREMENT PRIMARY KEY,
+  shorthand VARCHAR(25) DEFAULT NULL,
+  title VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS task (
+  task_id INT AUTO_INCREMENT PRIMARY KEY,
+  shorthand VARCHAR(25) DEFAULT NULL,
+  title VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS service (
+  service_id INT AUTO_INCREMENT PRIMARY KEY,
+  shorthand VARCHAR(25) DEFAULT NULL,
+  title VARCHAR(100) NOT NULL
+);
