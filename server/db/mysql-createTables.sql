450aec54875d210eb90d08109c0ac6feec33c601
Guenton <Guenton@Gmail.com>
2019-10-07 12:08:14 -0400

    database rebuild complete

--- server/db/mysql-createTables.sql
+++ server/db/mysql-createTables.sql
@@ -1,56 +1,118 @@
-CREATE TABLE IF NOT EXISTS employee (
-  emp_id INT AUTO_INCREMENT PRIMARY KEY,
-  firstname varchar(50) NOT NULL,
-  lastname varchar(50) NOT NULL,
-  pos_id INT DEFAULT NULL,
-  sub_id INT DEFAULT NULL,
+
+CREATE TABLE IF NOT EXISTS cost_curr (
+  costcurr_id INT AUTO_INCREMENT PRIMARY KEY,
+  iso CHAR(3), 
+  title VARCHAR(255),
   is_deleted BOOLEAN DEFAULT false,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+INSERT INTO cost_curr (iso, title) VALUES
+  ("USD", "United States dollar"),
+  ("ANG", "Netherlands Antillean guilder"),
+  ("AWG", "Aruban florin"),
+  ("EUR", "Euro")
+;
+CREATE TABLE cost_sig (
+  costsig_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  sig_limit DECIMAL(13, 2),
+  is_deleted BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+INSERT INTO cost_sig (title, sig_limit) VALUES
+  ("Management", 999999999.99),
+  ("CSO", 30000.00),
+  ("GOM", 2500.00),
+  ("GIM", 2500.00)
+;
+CREATE TABLE cost_type (
+  costtype_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  is_deleted BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+INSERT INTO cost_type (title) VALUES
+	("Telephone Expenses"),
+  ("Office Expenses"),
+  ("Operating Expenses"),
+  ("Transportation Expenses"),
+  ("Personnel Travel Expenses"),
+  ("Personnel Training Expenses"),
+  ("Personnel Other Expenses"),
+  ("Equipment Expenses"),
+  ("Repair & Maintenance Expenses")
+;
 
-CREATE TABLE IF NOT EXISTS sub (
-  sub_id INT AUTO_INCREMENT PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS scope_dept (
+  scopedept_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL
+);
+INSERT INTO scope_dept (title) VALUES
+  ("Within S&I"), ("Other Department"), ("Outside Bank")
+;
+CREATE TABLE IF NOT EXISTS scope_locat (
+  scopelocat_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL
+);
+INSERT INTO scope_locat (title) VALUES
+  ("Local"), ("Inter-Island"), ("International")
+;
+CREATE TABLE IF NOT EXISTS scope_sub (
+  scopesub_id INT AUTO_INCREMENT PRIMARY KEY,
   shorthand VARCHAR(25) NOT NULL,
-  location VARCHAR(25) NOT NULL,
+  country VARCHAR(100) NOT NULL,
   title VARCHAR(100) NOT NULL,
   is_deleted BOOLEAN DEFAULT false,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+INSERT INTO scope_sub (shorthand, country, title) VALUES
+  ("MCB", "Curacao", "Maduro & Curiel's Bank N.V."),
+  ("CMB", "Aruba", "Caribbean Mercantile Bank N.V."),
+  ("MCBB", "Bonaire", "Maduro & Curiel's Bank (Bonaire) N.V."),
+  ("WIB", "St. Maarten", "Windward Islands Bank Ltd.")
+;
 
-CREATE TABLE IF NOT EXISTS pos (
-  pos_id INT AUTO_INCREMENT PRIMARY KEY,
-  shorthand VARCHAR(25) DEFAULT NULL,
-  title VARCHAR(100) NOT NULL,
+
+CREATE TABLE IF NOT EXISTS serv_main (
+  servmain_id INT AUTO_INCREMENT PRIMARY KEY,
+  described VARCHAR(255) NOT NULL,
   is_deleted BOOLEAN DEFAULT false,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
-CREATE TABLE IF NOT EXISTS service_archtype (
-  arch_id INT AUTO_INCREMENT PRIMARY KEY,
-  shorthand VARCHAR(25) DEFAULT NULL,
-  archtype VARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS serv_reg (
+  servmain_id INT,
+  footprint INT,
+  cm_year YEAR,
+  cm_seq INT,
+  is_deleted BOOLEAN DEFAULT false,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (servmain_id) REFERENCES serv(servmain_id)
 );
-
-INSERT INTO service_archtype (shorthand, archtype)
-VALUES
+CREATE TABLE IF NOT EXISTS serv_arch (
+  servarch_id INT AUTO_INCREMENT PRIMARY KEY,
+  shorthand VARCHAR(25) DEFAULT NULL,
+  title VARCHAR(100) NOT NULL
+);
+INSERT INTO serv_arch (shorthand, title) VALUES
   ("Lead", "Security Leadership"),
   ("Inv", "Investigations"),
   ("Equip", "Security Equipment"),
   ("Prot", "Asset Protection"),
   ("Exec", "Executive Protection")
 ;
-
-CREATE TABLE IF NOT EXISTS service_type (
+CREATE TABLE IF NOT EXISTS serv_type (
   type_id INT AUTO_INCREMENT PRIMARY KEY,
+  servarch_id INT,
   title VARCHAR(255) NOT NULL,
-  arch_id INT,
-  FOREIGN KEY (arch_id) REFERENCES service_archtype(arch_id)
+  FOREIGN KEY (servarch_id) REFERENCES serv_arch(servarch_id)
 );
-
-INSERT INTO service_type (title, arch_id)
+INSERT INTO serv_type (title, servarch_id)
 VALUES
   ("Management", 1),
   ("Supervision", 1),
@@ -66,116 +128,75 @@ VALUES
   ("Transportation", 5),
   ("Domicile Protection", 5)
 ;
-
-CREATE TABLE IF NOT EXISTS normalas (
-  pos_id INT,
-  type_id INT,
-  PRIMARY KEY (pos_id, type_id),
-  FOREIGN KEY (pos_id) REFERENCES pos(pos_id),
-  FOREIGN KEY (type_id) REFERENCES service_type(type_id)
-);
-
-CREATE TABLE IF NOT EXISTS super_scope (
-  superscope_id INT AUTO_INCREMENT PRIMARY KEY,
-  superscope VARCHAR(255) NOT NULL
-);
-INSERT INTO super_scope (superscope) VALUES ("Local"), ("Inter-Island"), ("International");
-
-CREATE TABLE IF NOT EXISTS dep_scope (
-  depscope_id INT AUTO_INCREMENT PRIMARY KEY,
-  depscope VARCHAR(255) NOT NULL
-);
-INSERT INTO dep_scope (depscope) VALUES ("Within S&I"), ("Other Department"), ("Outside Bank");
-
-CREATE TABLE IF NOT EXISTS service_scope (
-  superscope_id INT,
-  depscope_id INT,
-  PRIMARY KEY (superscope_id, depscope_id),
-  FOREIGN KEY (superscope_id) REFERENCES super_scope(superscope_id),
-  FOREIGN KEY (depscope_id) REFERENCES dep_scope(depscope_id)
-);
-
-CREATE TABLE IF NOT EXISTS serv (
-  serv_id INT AUTO_INCREMENT PRIMARY KEY,
-  described VARCHAR(255) NOT NULL,
-  is_deleted BOOLEAN DEFAULT false,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS serv_typescope (
-  serv_id INT,
-  arch_id INT,
-  type_id INT,
-  depscope_id INT,
-  superscope_id INT,
+CREATE TABLE IF NOT EXISTS serv_scope (
+  servmain_id INT,
+  servarch_id INT,
+  servtype_id INT,
+  scopedept_id INT,
+  scopelocat_id INT,
   is_deleted BOOLEAN DEFAULT false,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (serv_id) REFERENCES serv(serv_id),
-  FOREIGN KEY (arch_id) REFERENCES service_archtype(arch_id),
-  FOREIGN KEY (type_id) REFERENCES service_type(type_id),
-  FOREIGN KEY (depscope_id) REFERENCES dep_scope(depscope_id),
-  FOREIGN KEY (superscope_id) REFERENCES super_scope(superscope_id)
+  FOREIGN KEY (servmain_id) REFERENCES serv_main(servmain_id),
+  FOREIGN KEY (servarch_id) REFERENCES serv_arch(servarch_id),
+  FOREIGN KEY (servtype_id) REFERENCES serv_type(servtype_id),
+  FOREIGN KEY (scopedept_id) REFERENCES scope_dept(scopedept_id),
+  FOREIGN KEY (scopelocat_id) REFERENCES scope_locat(scopelocat_id)
 );
 
-CREATE TABLE IF NOT EXISTS serv_reg (
-  serv_id INT,
-  footprint INT,
-  cm_year YEAR,
-  cm_seq INT,
+
+CREATE TABLE IF NOT EXISTS pos_main (
+  posmain_id INT AUTO_INCREMENT PRIMARY KEY,
+  shorthand VARCHAR(25) DEFAULT NULL,
+  title VARCHAR(100) NOT NULL,
   is_deleted BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS pos_assig (
+  posmain_id INT,
+  servtype_id INT,
+  PRIMARY KEY (posmain_id, servtype_id),
+  FOREIGN KEY (posmain_id) REFERENCES pos_main(posmain_id),
+  FOREIGN KEY (servtype_id) REFERENCES serv_type(servtype_id)
+);
+
+CREATE TABLE IF NOT EXISTS emp_main (
+  empmain_id INT AUTO_INCREMENT PRIMARY KEY,
+  posmain_id INT NOT NULL,
+  scopesub_id INT NOT NULL,
+  is_deleted BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (serv_id) REFERENCES serv(serv_id)
+  FOREIGN KEY (posmain_id) REFERENCES pos_main(posmain_id),
+  FOREIGN KEY (scopesub_id) REFERENCES scope_sub(scopesub_id)
 );
-
-CREATE TABLE cost_type (
-  costtype_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255),
-  is_deleted BOOLEAN DEFAULT false,
+CREATE TABLE IF NOT EXISTS emp_name (
+	empmain_id INT,
+  firstname VARCHAR(100),
+  middlename VARCHAR(100),
+  lastname VARCHAR(100),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (empmain_id) REFERENCES emp_main(empmain_id)
 );
-
-INSERT INTO cost_type (title) VALUES
-	("Telephone Expenses"),
-  ("Office Expenses"),
-  ("Operating Expenses"),
-  ("Transportation Expenses"),
-  ("Personnel Travel Expenses"),
-  ("Personnel Training Expenses"),
-  ("Personnel Other Expenses"),
-  ("Equipment Expenses"),
-  ("Repair & Maintenance Expenses")
-;
-
-CREATE TABLE cost_currency (
-  currency_id INT AUTO_INCREMENT PRIMARY KEY,
-  iso CHAR(3), 
-  title VARCHAR(255),
-  is_deleted BOOLEAN DEFAULT false,
+CREATE TABLE IF NOT EXISTS emp_email (
+	empmain_id INT,
+  email VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (empmain_id) REFERENCES emp_main(empmain_id)
 );
 
-INSERT INTO cost_currency (iso, title) VALUES
-  ("USD", "United States dollar"),
-  ("ANG", "Netherlands Antillean guilder"),
-  ("AWG", "Aruban florin"),
-  ("EUR", "Euro")
-;
 
-CREATE TABLE cost_signature (
-  costsig_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255),
-  sig_limit DECIMAL(13, 2),
-  is_deleted BOOLEAN DEFAULT false,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
 
-INSERT INTO cost_signature (title, sig_limit) VALUES
-  ("Management", 999999999.99),
-  ("CSO", 30000.00),
-  ("GOM", 2500.00),
-  ("GIM", 2500.00)
-;
+
+
+
+
+
+
+
+
+
+
+
